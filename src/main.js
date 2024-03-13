506da555afc97e504b37612fa8121208bacceae1
Hoomam <hoaz1600@student.miun.se>
2024-03-14 00:40:45 +0100

    fixat Charts.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 "use strcit"
 import nasdaqData from './nasdaq_screener.js'
+import testData from './test.js'
+import Chart from 'chart.js/auto'
 
 let searchInput = document.getElementById("stock-search");
 let autoComplete = document.getElementById("autoComplete"); 
@@ -50,18 +52,64 @@ function selectKeyWord(stock) {
 
 
 
-async function fetchData(stockSymbol) {
-   /*  try {
-        const response = await fetch(``);
+/* async function fetchData(stockSymbol) {
+    try {
+        const response = await fetch();
         const data = await response.json();
         console.log(data);
         
     } catch (error) {
         console.error('Fetch error:', error);
         throw error;
-    }  */
+    } 
     console.log(stockSymbol);  
     searchInput.value = ""; 
 }
+ */
 
 
+
+const priceHistory = testData.historical;
+
+// Assuming you've extracted the data into these arrays
+const datesArray = priceHistory.map((row) => row.date);
+const pricesArray = priceHistory.map((row) => row.close);
+
+new Chart(document.getElementById('priceChart'), {
+    type: 'line',
+    data: {
+        labels: datesArray,
+        datasets: [
+            {
+                label: 'Stock',
+                data: pricesArray,
+                borderWidth: 0.1
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+          scales: {
+            y: {
+                ticks: {
+                    stepSize: 50, 
+                    font: {
+                        size: 13,
+                        weight: 'bold',
+                    },
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    font: {
+                        size: 20,
+                        weight: 'bolder',
+                    },
+                },
+            },
+        },
+    },
+});
