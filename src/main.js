094329b2d25e24aa4495cbf146382b96da234509
Hoomam <hoaz1600@student.miun.se>
2024-03-14 17:44:03 +0100

    fetchProfile and showProfile

--- src/main.js
+++ src/main.js
@@ -4,6 +4,47 @@ import testData from './test.js';
 import testNews from './testNews.js';
 import Chart from 'chart.js/auto';
 
+const profile = [
+    {
+      "symbol": "AAPL",
+      "price": 174.005,
+      "beta": 1.289,
+      "volAvg": 58368390,
+      "mktCap": 2686967809500,
+      "lastDiv": 0.96,
+      "range": "151.64-199.62",
+      "changes": 2.875,
+      "companyName": "Apple Inc.",
+      "currency": "USD",
+      "cik": "0000320193",
+      "isin": "US0378331005",
+      "cusip": "037833100",
+      "exchange": "NASDAQ Global Select",
+      "exchangeShortName": "NASDAQ",
+      "industry": "Consumer Electronics",
+      "website": "https://www.apple.com",
+      "description": "Apple Inc. designs, manufactures, and markets smartphones, personal computers, tablets, wearables, and accessories worldwide. The company offers iPhone, a line of smartphones; Mac, a line of personal computers; iPad, a line of multi-purpose tablets; and wearables, home, and accessories comprising AirPods, Apple TV, Apple Watch, Beats products, and HomePod. It also provides AppleCare support and cloud services; and operates various platforms, including the App Store that allow customers to discover and download applications and digital content, such as books, music, video, games, and podcasts. In addition, the company offers various services, such as Apple Arcade, a game subscription service; Apple Fitness+, a personalized fitness service; Apple Music, which offers users a curated listening experience with on-demand radio stations; Apple News+, a subscription news and magazine service; Apple TV+, which offers exclusive original content; Apple Card, a co-branded credit card; and Apple Pay, a cashless payment service, as well as licenses its intellectual property. The company serves consumers, and small and mid-sized businesses; and the education, enterprise, and government markets. It distributes third-party applications for its products through the App Store. The company also sells its products through its retail and online stores, and direct sales force; and third-party cellular network carriers, wholesalers, retailers, and resellers. Apple Inc. was incorporated in 1977 and is headquartered in Cupertino, California.",
+      "ceo": "Mr. Timothy D. Cook",
+      "sector": "Technology",
+      "country": "US",
+      "fullTimeEmployees": "161000",
+      "phone": "408 996 1010",
+      "address": "One Apple Park Way",
+      "city": "Cupertino",
+      "state": "CA",
+      "zip": "95014",
+      "dcfDiff": 31.28305,
+      "dcf": 139.84694950805735,
+      "image": "https://financialmodelingprep.com/image-stock/AAPL.png",
+      "ipoDate": "1980-12-12",
+      "defaultImage": false,
+      "isEtf": false,
+      "isActivelyTrading": true,
+      "isAdr": false,
+      "isFund": false
+    }
+  ]; 
+
 let searchInput = document.getElementById("stock-search");
 let autoComplete = document.getElementById("autoComplete"); 
 //Matchar den sökta aktien med aktierna i nasdaq
@@ -44,8 +85,9 @@ function selectKeyWord(stock) {
         let stockName = chosenStock.Name;
         document.getElementById("search-button").addEventListener("click", function () {
             if(stockSymbol != ""){
-                fetchData(stockSymbol, stockName);
-                fetchNews(stockSymbol)
+                /* fetchData(stockSymbol, stockName);
+                fetchNews(stockSymbol) 
+                fetchProfile(stockSymbol)*/
                 stockSymbol = "";
             }
         });
@@ -88,7 +130,7 @@ new Chart(document.getElementById('priceChart'), {
         labels: datesArray.reverse(),
         datasets: [
             {
-                label: `${stockName}`,
+                /* label: `${stockName}`, */
                 data: pricesArray.reverse(),
                 borderWidth: 0.1
             },
@@ -122,6 +164,32 @@ new Chart(document.getElementById('priceChart'), {
 });
 }
 
+/* async function fetchProfile(stockSymbol) {
+    try {
+        const response = await fetch();
+        const data = await response.json();
+        showProfile(data);
+        
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }    
+}  */
+
+showProfile(profile);
+function showProfile(profile){
+   const profileDiv = document.getElementById("corpProfile");
+   profileDiv.innerHTML = "";
+   for(let item of profile){
+    profileDiv.innerHTML += `<p> Symbol: ${item.symbol}</p>`;
+    profileDiv.innerHTML += `<p> Price: ${item.price} $</p>`;
+    profileDiv.innerHTML += `<p> Industry: ${item.industry}</p>`;
+    profileDiv.innerHTML += `<p> Market Cap: ${item.mktCap} $</p>`;
+    profileDiv.innerHTML += `<p> CEO: ${item.ceo}</p>`;
+    profileDiv.innerHTML += `<p> Description: ${item.description}</p>`;
+    
+   }
+}
 
 /* async function fetchNews(stockSymbol) {
     try {
