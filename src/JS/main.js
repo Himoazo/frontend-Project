2ca83b2dff196a76c5ec4374767ceff62e733afc
Hoomam <hoaz1600@student.miun.se>
2024-03-18 12:32:35 +0100

    comment

--- src/JS/main.js
+++ src/JS/main.js
@@ -265,7 +265,7 @@ function showClearBtn() {
     showClearBtn();
   }
 
-
+//Ändrar margin top på footer så att det blir hela scroll längden
   const contentHeight = document.documentElement.scrollHeight;
   document.querySelector('footer').style.setProperty('--pageHeight', `${contentHeight}px`)
 
